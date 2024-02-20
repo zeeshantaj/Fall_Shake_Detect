8f3169447285ff5b85340425a58a7ca0c501472d
zeeshantaj <zeeshantaj.zt@gmail.com>
2024-02-20 15:07:42 +0500

    basic shake n fall commit

---
+++ app/src/main/java/com/example/shake_and_fall_check_app/Notification/ShakeFallNotificationService.java
@@ -0,0 +1,143 @@
+package com.example.shake_and_fall_check_app.Notification;
+
+import static com.example.shake_and_fall_check_app.Shake_and_Fall_Activity.FALL_THRESHOLD;
+import static com.example.shake_and_fall_check_app.Shake_and_Fall_Activity.SHAKE_SLOP_TIME_MS;
+import static com.example.shake_and_fall_check_app.Shake_and_Fall_Activity.SHAKE_THRESHOLD;
+import static com.example.shake_and_fall_check_app.Shake_and_Fall_Activity.lastShakeTime;
+import static com.example.shake_and_fall_check_app.Shake_and_Fall_Activity.lastX;
+import static com.example.shake_and_fall_check_app.Shake_and_Fall_Activity.lastY;
+import static com.example.shake_and_fall_check_app.Shake_and_Fall_Activity.lastZ;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+import com.example.shake_and_fall_check_app.R;
+
+public class ShakeFallNotificationService extends Service implements SensorEventListener {
+    private static final int NOTIFICATION_ID = 1;
+    private SensorManager sensorManager;
+    private Sensor accelerometerSensor;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            startForeground(NOTIFICATION_ID, new Notification());
+//        }
+
+        return START_STICKY;
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+     //   sensorManager.unregisterListener(this);
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            float acceleration = (float) Math.sqrt(x * x + y * y + z * z);
+
+            if (isShake(acceleration, x, y, z)) {
+                if (acceleration <= 60) {
+                    //handleShakeDetected();
+                    showNotification("Shake ","shaking detected "+acceleration);
+                    Log.e("MyApp", "shake acceleration " + acceleration);
+                }
+
+            } else if (acceleration > FALL_THRESHOLD) {
+                //handleFallDetected();
+                showNotification("Fall ","Fall detected "+acceleration);
+                Log.e("MyApp", "fall acceleration " + acceleration);
+            }
+
+            lastX = x;
+            lastY = y;
+            lastZ = z;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+    private boolean isShake(float acceleration, float x, float y, float z) {
+        // Implement your shake detection logic here
+        long now = System.currentTimeMillis();
+        if (acceleration > SHAKE_THRESHOLD && now - lastShakeTime > SHAKE_SLOP_TIME_MS) {
+            float deltaX = Math.abs(x - lastX);
+            float deltaY = Math.abs(y - lastY);
+            float deltaZ = Math.abs(z - lastZ);
+
+            if ((deltaX > 5 && deltaY > 5) || (deltaX > 5 && deltaZ > 5) || (deltaY > 5 && deltaZ > 5)) {
+                if (acceleration >= 41) {
+                    lastShakeTime = now;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private void showNotification(String title, String message) {
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    "channel_id",
+                    "Channel Name",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        Intent dismissIntent = new Intent(NotificationManager.ACTION_NOTIFICATION_CANCELLED);
+        PendingIntent dismissPendingIntent = PendingIntent.getBroadcast(this, 0, dismissIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "channel_id")
+                .setSmallIcon(R.drawable.baseline_notifications_active_24)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setDeleteIntent(dismissPendingIntent); // Set the delete intent
+
+        startForeground(NOTIFICATION_ID, builder.build());
+
+//        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "channel_id")
+//                .setSmallIcon(R.drawable.baseline_notifications_active_24)
+//                .setContentTitle(title)
+//                .setContentText(message)
+//                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+//        notificationManager.cancel(NOTIFICATION_ID);
+//
+//        startForeground(NOTIFICATION_ID, builder.build());
+
+    }
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+}
--- app/src/main/java/com/example/shake_and_fall_check_app/Shake_and_Fall_Activity.java
+++ app/src/main/java/com/example/shake_and_fall_check_app/Shake_and_Fall_Activity.java
@@ -3,6 +3,8 @@ package com.example.shake_and_fall_check_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -12,20 +14,22 @@ import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.Toast;
 
+import com.example.shake_and_fall_check_app.Notification.ShakeFallNotificationService;
 import com.google.android.material.materialswitch.MaterialSwitch;
 
 public class Shake_and_Fall_Activity extends AppCompatActivity {
-    private static final float FALL_THRESHOLD = 40.8f * 2; // 2g threshold for fall detection
-    private static final int SHAKE_SLOP_TIME_MS = 500; // minimum time between two shake events
-    private static final float SHAKE_THRESHOLD = 29; // acceleration threshold for shake detection
+    public static final float FALL_THRESHOLD = 40.8f * 2; // 2g threshold for fall detection
+    public static final int SHAKE_SLOP_TIME_MS = 500; // minimum time between two shake events
+    public static final float SHAKE_THRESHOLD = 29; // acceleration threshold for shake detection
 
-    private long lastShakeTime = 0;
-    private float lastX, lastY, lastZ;
+    public static long lastShakeTime = 0;
+    public static float lastX, lastY, lastZ;
 
     private SensorManager sensorManager;
     private Sensor accelerometerSensor;
 
     private MaterialSwitch detectSwitchBtn;
+    private SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,79 +38,105 @@ public class Shake_and_Fall_Activity extends AppCompatActivity {
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
-
+        sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
         detectSwitchBtn = findViewById(R.id.detectSwitch);
-        detectSwitchBtn.setChecked(false);
-        unregisterSensorListener();
+        detectSwitchBtn.setChecked(sharedPreferences.getBoolean("switchState", false));
+        boolean isCheckedInitial = sharedPreferences.getBoolean("switchState", false);
+        if (isCheckedInitial) {
+            Toast.makeText(this, "Active", Toast.LENGTH_SHORT).show();
+            startService(new Intent(this,ShakeFallNotificationService.class));
+        } else {
+            Toast.makeText(this, "InActive", Toast.LENGTH_SHORT).show();
+            stopService(new Intent(this,ShakeFallNotificationService.class));
+        }
+
 
         detectSwitchBtn.setOnCheckedChangeListener((buttonView, isChecked) -> {
+
             if (isChecked) {
                 Toast.makeText(this, "Active", Toast.LENGTH_SHORT).show();
-                registerSensorListener();
+                //registerSensorListener();
+                startService(new Intent(this,ShakeFallNotificationService.class));
             } else {
                 Toast.makeText(this, "InActive", Toast.LENGTH_SHORT).show();
-                unregisterSensorListener();
+                stopService(new Intent(this,ShakeFallNotificationService.class));
             }
+            sharedPreferences.edit().putBoolean("switchState", isChecked).apply();
+
+
+            //            if (isChecked) {
+//
+//
+//                Intent intent = new Intent(this, ShakeFallNotificationService.class);
+//                startService(intent);
+////                Toast.makeText(this, "Active", Toast.LENGTH_SHORT).show();
+////                registerSensorListener();
+//            } else {
+//                Intent intent = new Intent(this, ShakeFallNotificationService.class);
+//                stopService(intent);
+////                Toast.makeText(this, "InActive", Toast.LENGTH_SHORT).show();
+////                unregisterSensorListener();
+//            }
         });
     }
 
 
-    private void registerSensorListener() {
-        sensorManager.registerListener(sensorEventListener, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    private void unregisterSensorListener() {
-        sensorManager.unregisterListener(sensorEventListener);
-    }
-    private final SensorEventListener sensorEventListener = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-                if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-                    float x = event.values[0];
-                    float y = event.values[1];
-                    float z = event.values[2];
-
-                float acceleration = (float) Math.sqrt(x * x + y * y + z * z);
-
-                if (isShake(acceleration, x, y, z)) {
-                    if (acceleration <= 60) {
-                        handleShakeDetected();
-                        Log.e("MyApp", "shake acceleration " + acceleration);
-                    }
-
-                } else if (acceleration > FALL_THRESHOLD) {
-                    handleFallDetected();
-                    Log.e("MyApp", "fall acceleration " + acceleration);
-                }
-
-                lastX = x;
-                lastY = y;
-                lastZ = z;
-            }
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-            // Not used, but required to implement SensorEventListener
-        }
-
-        private boolean isShake(float acceleration, float x, float y, float z) {
-            long now = System.currentTimeMillis();
-            if (acceleration > SHAKE_THRESHOLD && now - lastShakeTime > SHAKE_SLOP_TIME_MS) {
-                float deltaX = Math.abs(x - lastX);
-                float deltaY = Math.abs(y - lastY);
-                float deltaZ = Math.abs(z - lastZ);
-
-                if ((deltaX > 5 && deltaY > 5) || (deltaX > 5 && deltaZ > 5) || (deltaY > 5 && deltaZ > 5)) {
-                    if (acceleration >= 41) {
-                        lastShakeTime = now;
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-    };
+//    private void registerSensorListener() {
+//        sensorManager.registerListener(sensorEventListener, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+//    }
+//
+//    private void unregisterSensorListener() {
+//        sensorManager.unregisterListener(sensorEventListener);
+//    }
+//    private final SensorEventListener sensorEventListener = new SensorEventListener() {
+//        @Override
+//        public void onSensorChanged(SensorEvent event) {
+//                if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+//                    float x = event.values[0];
+//                    float y = event.values[1];
+//                    float z = event.values[2];
+//
+//                float acceleration = (float) Math.sqrt(x * x + y * y + z * z);
+//
+//                if (isShake(acceleration, x, y, z)) {
+//                    if (acceleration <= 60) {
+//                        handleShakeDetected();
+//                        Log.e("MyApp", "shake acceleration " + acceleration);
+//                    }
+//
+//                } else if (acceleration > FALL_THRESHOLD) {
+//                    handleFallDetected();
+//                    Log.e("MyApp", "fall acceleration " + acceleration);
+//                }
+//
+//                lastX = x;
+//                lastY = y;
+//                lastZ = z;
+//            }
+//        }
+//
+//        @Override
+//        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+//            // Not used, but required to implement SensorEventListener
+//        }
+//
+//        private boolean isShake(float acceleration, float x, float y, float z) {
+//            long now = System.currentTimeMillis();
+//            if (acceleration > SHAKE_THRESHOLD && now - lastShakeTime > SHAKE_SLOP_TIME_MS) {
+//                float deltaX = Math.abs(x - lastX);
+//                float deltaY = Math.abs(y - lastY);
+//                float deltaZ = Math.abs(z - lastZ);
+//
+//                if ((deltaX > 5 && deltaY > 5) || (deltaX > 5 && deltaZ > 5) || (deltaY > 5 && deltaZ > 5)) {
+//                    if (acceleration >= 41) {
+//                        lastShakeTime = now;
+//                        return true;
+//                    }
+//                }
+//            }
+//            return false;
+//        }
+//    };
 
     private void handleFallDetected() {
         // Notify that a fall has been detected
@@ -118,15 +148,15 @@ public class Shake_and_Fall_Activity extends AppCompatActivity {
         Toast.makeText(this, "Shake detected!", Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        sensorManager.registerListener(sensorEventListener, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        sensorManager.unregisterListener(sensorEventListener);
-    }
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        sensorManager.registerListener(sensorEventListener, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        sensorManager.unregisterListener(sensorEventListener);
+//    }
 }
